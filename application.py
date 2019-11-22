63be1be685cc79d5ea9ef0de9d05669f3fe261ba
Viola <hustlaviola@gmail.com>
2019-11-22 13:19:02 +0100

    integrate database

--- application.py
+++ application.py
@@ -1,9 +1,36 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '2417f062814e6d7c0b2108c9abc24109'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meetup.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='no-img.png')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='owner', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.posted_at}', '{self.owner_id}')"
+
+
 
 posts = [
     {
