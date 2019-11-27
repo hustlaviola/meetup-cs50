f7eeafd4bcc482754418de050aadc0852e81ddf1
Viola <hustlaviola@gmail.com>
2019-11-27 05:37:23 +0100

    Create error pages

--- meetup/__init__.py
+++ meetup/__init__.py
@@ -25,9 +25,11 @@ def create_app(config_class=Config):
     from meetup.users.routes import users
     from meetup.posts.routes import posts
     from meetup.main.routes import main
+    from meetup.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
